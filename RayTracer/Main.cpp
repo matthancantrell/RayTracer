4ef374f4c39dcc1b6d6c6eee16e0e44e43f83c6d
Matthan Cantrell <cantrellmatthan247@gmail.com>
2022-12-08 08:31:28 -0700

    Plane Class Added!

--- RayTracer/Main.cpp
+++ RayTracer/Main.cpp
@@ -2,6 +2,7 @@
 #include "Renderer/Renderer.h"
 #include "Renderer/Canvas.h"
 #include "Objects/Sphere.h"
+#include "Objects/Plane.h"
 #include "Objects/Scene.h"
 #include "Renderer/Camera.h"
 
@@ -24,7 +25,7 @@ int main(int, char**)
 
     scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 0, -1 }, 0.5f, std::make_unique<Lambertian>(color3{ 0, 1, 0 })));
     scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 1, 0, -1 }, 0.5f, std::make_unique<Metal>(color3{ 1, 1, 1 }, 0.1f)));
-    scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, -100.5, -1 }, 100.0f, std::make_unique<Lambertian>(color3{ 0.2f, 0.2f, 0.2f })));
+    scene.AddObject(std::make_unique<Plane>(glm::vec3{ 0, -0.5, 0 }, glm::vec3{ 0, 1, 0 }, std::make_unique<Lambertian>(color3{ 0.2f, 0.2f, 0.2f })));
 
     bool quit = false;
     while (!quit)
